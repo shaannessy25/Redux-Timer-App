6fc99fa9c486099ef34de3f0a9f69d0469528c81
shaannessy25 <47993680+shaannessy25@users.noreply.github.com>
2020-06-22 14:25:27 -0700

    adding timer class

---
+++ src/Timer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+class Timer {
+    constructor(name) {
+        this.name = name;
+        this.time = 0;
+        this.isRunning  = false;
+    }
+}
+
+
+export default Timer;
